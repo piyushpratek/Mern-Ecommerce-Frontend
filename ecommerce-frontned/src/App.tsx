ca926651402933498b3ecf24d1a6363730654216
Piyush Prateek <piyushprateek231996@gmail.com>
2024-05-31 23:23:57 +0530

    Admin section- commit

--- ecommerce-frontned/src/App.tsx
+++ ecommerce-frontned/src/App.tsx
@@ -1,7 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loader from "./components/loader";
+
+
+const Home = lazy(() => import("./pages/home"));
+const Search = lazy(() => import("./pages/search"));
+const Cart = lazy(() => import("./pages/cart"));
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+
+      <Suspense fallback={<Loader />}>
+
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+
+        </Routes>
+
+      </Suspense>
+    </Router>
   )
 }
 
