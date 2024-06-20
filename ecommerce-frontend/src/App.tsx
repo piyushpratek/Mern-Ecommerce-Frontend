81bf168484cf53b3e616e23d362cadf2b244c095
Piyush Prateek <piyushprateek231996@gmail.com>
2024-06-20 23:22:26 +0530

    App- included Login route

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -9,6 +9,7 @@ const Home = lazy(() => import("./pages/Home"));
 const Search = lazy(() => import("./pages/Search"));
 const Cart = lazy(() => import("./pages/Cart"));
 const Shipping = lazy(() => import("./pages/Shipping"));
+const Login = lazy(() => import("./pages/Login"));
 
 
 // Admin Routes Importing
@@ -41,6 +42,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+          {/* Not Logged in Route  */}
+          <Route path="/login" element={<Login />} />
 
           {/* Logged in user routes  */}
           <Route>
