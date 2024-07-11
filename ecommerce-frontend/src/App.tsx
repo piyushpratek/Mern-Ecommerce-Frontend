52aba0bc32c7fa676edf4da1241e5f5590e4e8e8
Piyush Prateek <piyushprateek231996@gmail.com>
2024-07-11 14:59:49 +0530

    redux - folder added store.ts file added in main.tsx - wrapped app with provider firebase.ts - file added app .tsx -toaster added .env file added

--- ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/src/App.tsx
@@ -1,9 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
-
 import Loader from "./components/Loader";
 import Header from "./components/Header";
-
+import { Toaster } from "react-hot-toast"
 
 const Home = lazy(() => import("./pages/Home"));
 const Search = lazy(() => import("./pages/Search"));
@@ -94,6 +93,7 @@ const App = () => {
         </Routes>
 
       </Suspense>
+      <Toaster position="bottom-center" />
     </Router>
   )
 }
--- ecommerce-frontend/src/pages/Login.tsx
+++ ecommerce-frontend/src/pages/Login.tsx
@@ -1,8 +1,8 @@
-// import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useState } from "react";
-// import toast from "react-hot-toast";
-// import { FcGoogle } from "react-icons/fc";
-// import { auth } from "../firebase";
+import toast from "react-hot-toast";
+import { FcGoogle } from "react-icons/fc";
+import { auth } from "../firebase";
 // import { getUser, useLoginMutation } from "../redux/api/userAPI";
 // import { FetchBaseQueryError } from "@reduxjs/toolkit/query/react";
 // import { MessageResponse } from "../types/api-types";
@@ -16,45 +16,46 @@ const Login = () => {
 
   // const [login] = useLoginMutation();
 
-  // const loginHandler = async () => {
-  //   try {
-  //     const provider = new GoogleAuthProvider();
-  //     const { user } = await signInWithPopup(auth, provider);
+  const loginHandler = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const { user } = await signInWithPopup(auth, provider);
 
-  //     console.log({
-  //       name: user.displayName!,
-  //       email: user.email!,
-  //       photo: user.photoURL!,
-  //       gender,
-  //       role: "user",
-  //       dob: date,
-  //       _id: user.uid,
-  //     });
+      console.log({
+        name: user.displayName!,
+        email: user.email!,
+        photo: user.photoURL!,
+        gender,
+        role: "user",
+        dob: date,
+        _id: user.uid,
+      });
 
-  //     const res = await login({
-  //       name: user.displayName!,
-  //       email: user.email!,
-  //       photo: user.photoURL!,
-  //       gender,
-  //       role: "user",
-  //       dob: date,
-  //       _id: user.uid,
-  //     });
+      // const res = await login({
+      //   name: user.displayName!,
+      //   email: user.email!,
+      //   photo: user.photoURL!,
+      //   gender,
+      //   role: "user",
+      //   dob: date,
+      //   _id: user.uid,
+      // });
 
-  //     if ("data" in res) {
-  //       toast.success(res.data.message);
-  //       const data = await getUser(user.uid);
-  //       dispatch(userExist(data?.user!));
-  //     } else {
-  //       const error = res.error as FetchBaseQueryError;
-  //       const message = (error.data as MessageResponse).message;
-  //       toast.error(message);
-  //       dispatch(userNotExist());
-  //     }
-  //   } catch (error) {
-  //     toast.error("Sign In Fail");
-  //   }
-  // };
+      // if ("data" in res) {
+      //   toast.success(res.data.message);
+      //   const data = await getUser(user.uid);
+      //   dispatch(userExist(data?.user!));
+      // }
+      //  else {
+      //   const error = res.error as FetchBaseQueryError;
+      //   const message = (error.data as MessageResponse).message;
+      //   toast.error(message);
+      //   dispatch(userNotExist());
+      // }
+    } catch (error) {
+      toast.error("Sign In Fail");
+    }
+  };
 
   return (
     <div className="login">
@@ -81,9 +82,9 @@ const Login = () => {
 
         <div>
           <p>Already Signed In Once</p>
-          {/* <button onClick={loginHandler}>
+          <button onClick={loginHandler}>
             <FcGoogle /> <span>Sign in with Google</span>
-          </button> */}
+          </button>
         </div>
       </main>
     </div>
